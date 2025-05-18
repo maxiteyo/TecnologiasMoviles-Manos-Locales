5fb0d43810183459e3f951df49302d039b36e8b3
Maximo Teyo <maxiiteyo@gmail.com>
2025-05-18 15:35:58 -0300

    mejoras de interfaz

--- app/src/main/java/com/example/manoslocales/MainActivity.kt
+++ app/src/main/java/com/example/manoslocales/MainActivity.kt
@@ -1,20 +1,10 @@
 package com.example.manoslocales
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.manoslocales.ui.theme.ManosLocalesTheme
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import android.content.Context
 

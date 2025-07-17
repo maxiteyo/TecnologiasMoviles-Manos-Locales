51e997090499ba1848746952e9c4b5c799cdb0ce
Sofia Suppia <sofimisuppia@gmail.com>
2025-07-17 19:48:09 -0300

    compartir

--- app/src/main/java/com/example/manoslocales/ProductDetailFragment.kt
+++ app/src/main/java/com/example/manoslocales/ProductDetailFragment.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
+import com.example.manoslocales.utils.shareGeneric
+import com.example.manoslocales.utils.shareOnInstagram
+import com.example.manoslocales.utils.shareOnWhatsApp
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class ProductDetailFragment : Fragment() {
 
@@ -32,5 +38,46 @@ class ProductDetailFragment : Fragment() {
             .into(imageView) // Carga la imagen en el ImageView
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val instagramButton = view.findViewById<ImageButton>(R.id.btnInstagram)
+        val whatsAppButton = view.findViewById<ImageButton>(R.id.btnWp)
+        val shareButton = view.findViewById<ImageButton>(R.id.btnShare)
+
+        instagramButton.setOnClickListener {
+            // Lanza una corrutina para llamar a la función suspendida
+            lifecycleScope.launch {
+                shareOnInstagram(requireContext(), args.productImageUrl)
+            }
+        }
+
+        whatsAppButton.setOnClickListener {
+            // Lanza una corrutina para llamar a la función suspendida
+            lifecycleScope.launch {
+                shareOnWhatsApp(
+                    context = requireContext(),
+                    productName = args.productName,
+                    productDescription = args.productDescription,
+                    producerName = args.producerName, // Pasa el nombre del productor
+                    productImageUrl = args.productImageUrl
+                )
+            }
+        }
+
+        shareButton.setOnClickListener {
+            // Lanza una corrutina para llamar a la función suspendida
+            lifecycleScope.launch {
+                shareGeneric(
+                    context = requireContext(),
+                    productName = args.productName,
+                    productDescription = args.productDescription,
+                    producerName = args.producerName, // Pasa el nombre del productor
+                    productImageUrl = args.productImageUrl
+                )
+            }
+        }
     }
 }
